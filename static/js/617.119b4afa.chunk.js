a38c905cd26c46a82d4f142018d6a337558bf2c7
Dzenkinstone <kozlovdanil6@gmail.com>
2023-07-10 17:14:16 +0300

    Updates

---
+++ static/js/617.119b4afa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkweather_forecast=self.webpackChunkweather_forecast||[]).push([[617],{8617:function(t,e,r){r.d(e,{Bhs:function(){return a}});var n=r(9983);function a(t){return(0,n.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",strokeWidth:"2",stroke:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",d:"M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"}}]})(t)}}}]);
+//# sourceMappingURL=617.119b4afa.chunk.js.map
